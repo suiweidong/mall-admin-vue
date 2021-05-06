8fad1ef50e728b7d5e5cccf3c5e63e2746ffda9f
suiweidong <7334501@qq.com>
2021-05-06 09:43:15 +0800

    这是商城后台管理前端项目

---
+++ src/views/common/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mod-home">
+    <h3>东哥哥商城后台管理平台</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+  .mod-home {
+    line-height: 1.5;
+  }
+</style>
+
